f3bcad850d9065facb8ac70fc920956771379cb8
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2020-12-23 11:31:35 +0000

    quickstart added

---
+++ quickstart/README.md
@@ -0,0 +1,13 @@
+# Quickstart
+
+Implementation of the [Quickstart in the Solace PubSub+ Ansible Collection documentation](https://solace-iot-team.github.io/ansible-solace-collection/quickstart.html).
+Follow the installation steps in the quickstart.
+
+## Run
+
+````bash
+./run.sh
+````
+
+
+---
---
+++ quickstart/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+scriptDir=$(cd $(dirname "$0") && pwd);
+
+# set the python interpreter
+  export ANSIBLE_PYTHON_INTERPRETER=$(python3 -c "import sys; print(sys.executable)")
+
+# set verbosity
+  export ANSIBLE_VERBOSITY=3
+
+# set the working dir
+  WORKING_DIR="$scriptDir/tmp"
+
+# create broker service
+  ansible-playbook "$scriptDir/service.playbook.yml" --extra-vars "WORKING_DIR=$WORKING_DIR"
+
+# configure broker service
+  # enable logging
+    export ANSIBLE_SOLACE_ENABLE_LOGGING=True
+    export ANSIBLE_SOLACE_LOG_PATH="$WORKING_DIR/ansible-solace.log"
+  ansible-playbook -i "$WORKING_DIR/broker.inventory.yml" "$scriptDir/configure.playbook.yml"
