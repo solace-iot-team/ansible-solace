dda69990fbf7a4e7e7d532fbc68543efea8666fe
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2020-12-29 11:03:37 +0000

    upgrade to latest ansible-solace

--- working-with-users-and-acls/run.sh
+++ working-with-users-and-acls/run.sh
@@ -13,9 +13,11 @@ scriptDir=$(cd $(dirname "$0") && pwd);
 
 # create broker service
   ansible-playbook "$scriptDir/service.playbook.yml" --extra-vars "WORKING_DIR=$WORKING_DIR"
+  code=$?; if [[ $code != 0 ]]; then echo ">>> ERROR - $code"; exit 1; fi
 
 # configure broker service
   # enable logging
     export ANSIBLE_SOLACE_ENABLE_LOGGING=True
     export ANSIBLE_SOLACE_LOG_PATH="$WORKING_DIR/ansible-solace.log"
   ansible-playbook -i "$WORKING_DIR/broker.inventory.yml" "$scriptDir/configure.playbook.yml"  --extra-vars "AUTO_RUN=$AUTO_RUN"
+  code=$?; if [[ $code != 0 ]]; then echo ">>> ERROR - $code"; exit 1; fi
